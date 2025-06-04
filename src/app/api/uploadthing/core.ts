687f7b19e0742c1a59ff79eff46522481f9f68d8
malcolm staso <malcolmstaso@malcolms-MacBook-Pro.local>
2025-06-04 18:27:53 -0400

    2025_June4_upload other than image

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,33 +1,47 @@
 import { auth } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
-import { MUTATIONS } from "~/server/db/queries";
+import z from "zod";
+import { MUTATIONS, QUERIES } from "~/server/db/queries";
 
 const f = createUploadthing();
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({
-    image: {
+  driveUploader: f({
+    blob: {
       /**
        * For full list of options and defaults, see the File Route API reference
        * @see https://docs.uploadthing.com/file-routes#route-config
        */
-      maxFileSize: "4MB",
-      maxFileCount: 1,
+      maxFileSize: "1GB",
+      maxFileCount: 9999,
     },
   })
+    .input(
+      z.object({
+        folderId: z.number(),
+      }),
+    )
     // Set permissions and file types for this FileRoute
-    .middleware(async () => {
+    .middleware(async ({ input }) => {
       // This code runs on your server before upload
       const user = await auth();
+      console.log("been hit user");
 
       // If you throw, the user will not be able to upload
       if (!user.userId) throw new UploadThingError("Unauthorized");
 
+      const folder = await QUERIES.getFolderById(input.folderId);
+
+      if (!folder) throw new UploadThingError("Folder not found");
+
+      //   if (folder.ownerId !== user.userId)
+      //     throw new UploadThingError("Unauthorized");
+
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.userId };
+      return { userId: user.userId, parentId: input.folderId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
@@ -40,7 +54,7 @@ export const ourFileRouter = {
           name: file.name,
           size: file.size,
           url: file.ufsUrl,
-          parent: 4503599627370498,
+          parent: metadata.parentId,
         },
         userId: metadata.userId,
       });
